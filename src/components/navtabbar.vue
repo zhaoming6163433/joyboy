41cfa55377c7113c70401047050f2a6d11c04be3
JDJR\zhaoming16 <zhaoming16@jd.com>
2019-09-22 11:15:39 +0800

    抄底

--- src/components/navtabbar.vue
+++ src/components/navtabbar.vue
@@ -13,8 +13,8 @@
           <div class="barimg shoppingcart img3"></div>
           背包
         </mt-tab-item>
-        <mt-tab-item id="share">
-          <div class="barimg my img4"></div>
+        <mt-tab-item id="share" >
+          <div class="barimg my img4" @click="share"></div>
           分享
         </mt-tab-item>
       </mt-tabbar>
@@ -40,13 +40,12 @@ export default {
                 this.$router.push({"name":"sharkpage"})
                 break;
             case 'task':
-              
+                this.$router.push({"name":"task"})
                 break;
             case 'package':
                 this.$router.push({"name":"package"})
                 break;
             case 'share':
-              
                 break;
           }
       }
@@ -58,6 +57,26 @@ export default {
     });
   },
   methods : {
+      share(){
+          JrBridge.callNative({
+            "type":"4",
+            "shareDataNew":{
+                "isLogin":"0",
+                "id":"5",
+                "linkSubtitle":"救命我变成狗了",
+                "imageUrl":"http://pre-imlab-midas.jd.com/joyboy/static/img/joy_still_new.dcfc6d32.png",
+                "link":[
+                    "http://pre-imlab-midas.jd.com/joyboy"
+                ],
+                "linkTitle":"inJOY ZONE",
+                "channels":["0","1","4","5"],
+                "productName":"jijin_xiangqing",
+                "productId":"100487"
+            }
+        },function(res){
+            console.log('res from native=>',res);
+        });
+      }
   },
   mounted(){
     
